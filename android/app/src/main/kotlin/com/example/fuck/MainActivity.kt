7570f484e04e9e5c86ee13bb7d4bd2888c9731f4
Iman <imankeramati1401@gmail.com>
2025-06-03 13:16:04 +0330

    fix project bug

--- android/app/src/main/kotlin/com/example/fuck/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.baftopia
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/keramati/baftopia/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.keramati.baftopia
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
